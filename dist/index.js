7f284ba0ab60fb7b12ffd47a38dd9f1caf8296ae
Rishichandra Wawhal <rishi@hasura.io>
2021-09-26 20:13:08 +0530

    true

--- dist/index.js
+++ dist/index.js
@@ -4877,7 +4877,6 @@ const doesProjectExist = (appName, client) => __awaiter(void 0, void 0, void 0, 
                 name: appName
             }
         });
-        console.log(resp);
         if (resp.projects.length) {
             return true;
         }
@@ -6830,7 +6829,7 @@ function run() {
                 REGION: 'us-east-2',
                 NAME: 'tenant1',
                 GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
-                HASURA_CLOUD_PAT: 'XGytdW2Ew7vDhH6YzO6c1LUGpLTUziNR50c01sGnZCi7K3Vx31fpP61dAw4gbUNIa',
+                HASURA_CLOUD_PAT: 'XGytdW2Ew7vDhH6YzO6c1LUGpLTUziNR50c01sGnZCi7K3Vx31fpP61dAw4gbUNI',
                 CLOUD_DATA_GRAPHQL: 'https://2a8e-106-51-72-39.ngrok.io/v1/graphql'
             };
             const outputVars = yield handler(params);

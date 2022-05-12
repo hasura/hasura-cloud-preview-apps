b3560cbb4d574080d1c0e70f237886ffe54cd65f
Ram <ram@hasura.io>
2022-05-12 18:30:40 +0530

    check sslmode in connection string

--- src/postgres.ts
+++ src/postgres.ts
@@ -40,12 +40,17 @@ export const createEphemeralDb = async (
   connectionString: string,
   dbName: string
 ) => {
-  const pgClient = new Client({
-    connectionString,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
+  const connectionParams = connectionString.includes('?sslmode=require')
+    ? {
+        connectionString: connectionString.replace('?sslmode=require', ''),
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {connectionString}
+
+  const pgClient = new Client(connectionParams)
+
   try {
     await dropAndCreateDb(dbName, pgClient)
   } catch (e) {
